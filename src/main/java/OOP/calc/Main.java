cd2abfd3b2f6fdfc983ede638f276d249ae3f7ef
lekondrateva <umpalus@gmail.com>
2022-06-28 09:58:22 +0500

    ввод выражения из консоли добавлен в Main

--- src/main/java/OOP/calc/Main.java
+++ src/main/java/OOP/calc/Main.java
@@ -1,9 +1,12 @@
 package OOP.calc;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String expression = "11 / 5";
-
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите выражения для вычисления в формате 'a + b':");
+        String expression = in.nextLine();
         ReadExp readExp = new ReadExp(expression);
         Calculator calculator = new Calculator(readExp.getA(), readExp.getOperation(), readExp.getB());
         System.out.println(calculator.calculate().getResult());
